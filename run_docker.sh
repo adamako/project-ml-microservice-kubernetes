0dab030b8f578ec59183905edff2dd674eecf0e2
Adama KO <adamako34@hotmail.com>
2022-07-22 11:47:56 +0000

    task:complete docker run script

--- run_docker.sh
+++ run_docker.sh
@@ -3,10 +3,10 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+docker build -t prediction-app .
 
 # Step 2: 
-# List docker images
+docker images
 
 # Step 3: 
-# Run flask app
+docker run -p 8000:80 prediction-app
