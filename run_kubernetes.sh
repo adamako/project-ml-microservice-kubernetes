505283f36648e4887849bea157eec76401778700
Adama KO <adamako34@hotmail.com>
2022-07-22 17:28:16 +0000

    task: complete deployment script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,19 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+#This is your Docker ID/path
+dockerpath="adamako/prediction-app:latest"
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run prediction-app\
+    --image=$dockerpath\
+    --port=80 --labels app=prediction-app
 
 
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
+kubectl port-forward prediction-app 8000:80
+
 
